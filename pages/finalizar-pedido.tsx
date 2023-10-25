39cd70baac14f0f42521cf7e6a8640cbdbde17fb
miguelalonso22 <miguelalonsotormes@gmail.com>
2023-10-25 10:28:56 -0300

    fixed-import(for good)

--- pages/finalizar-pedido.tsx
+++ pages/finalizar-pedido.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 import React, { Fragment, useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-import { Modal } from "../components/Modal";
+import { Modal } from "../components";
 
 
 // Define una interface para los parámetros de la query
